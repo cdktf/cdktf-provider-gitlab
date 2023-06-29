4e27f0b46bc5997da6adf019f4806abeb7f2de2f
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-06-29 15:40:14 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/integration-emails-on-push/index.ts
+++ src/integration-emails-on-push/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/gitlabhq/gitlab/16.1.0/docs/resources/integration_emails_on_push
 // generated from terraform resource schema
 
