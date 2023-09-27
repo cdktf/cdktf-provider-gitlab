091d43f8ea39d6b26faef126253981360d6b1476
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2023-09-27 08:31:27 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-gitlab-group-membership/index.ts
+++ src/data-gitlab-group-membership/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/gitlabhq/gitlab/16.4.1/docs/data-sources/group_membership
 // generated from terraform resource schema
 
