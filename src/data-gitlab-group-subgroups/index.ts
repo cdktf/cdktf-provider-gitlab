d57006ee6c9996968b4a8dc52633042133c82e7d
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-08-09 11:55:32 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@9b725cd

--- src/data-gitlab-group-subgroups/index.ts
+++ src/data-gitlab-group-subgroups/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/gitlabhq/gitlab/17.2.0/docs/data-sources/group_subgroups
 // generated from terraform resource schema
 
