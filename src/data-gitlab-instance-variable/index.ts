851e795d0624c426c6de859d1e081ad03ed982ae
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-09-15 05:58:33 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@321d292

--- src/data-gitlab-instance-variable/index.ts
+++ src/data-gitlab-instance-variable/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/gitlabhq/gitlab/17.3.1/docs/data-sources/instance_variable
 // generated from terraform resource schema
 
