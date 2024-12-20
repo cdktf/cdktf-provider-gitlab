4431a4a88b94e4c8e358ecdadcd984c6004f6f12
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-12-20 17:30:26 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/group-security-policy-attachment/index.ts
+++ src/group-security-policy-attachment/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/gitlabhq/gitlab/17.7.0/docs/resources/group_security_policy_attachment
 // generated from terraform resource schema
 
