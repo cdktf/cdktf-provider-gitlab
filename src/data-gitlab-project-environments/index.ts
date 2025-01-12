34fba7fa1c47dbe8faae01527ad7950d916be7c3
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-12 05:59:59 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/data-gitlab-project-environments/index.ts
+++ src/data-gitlab-project-environments/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/gitlabhq/gitlab/17.7.1/docs/data-sources/project_environments
 // generated from terraform resource schema
 
