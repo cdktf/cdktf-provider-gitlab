a4a4a6d524cda0c1a56a78294dcf83866a5bd9e9
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-01-19 05:55:16 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@8638bf9

--- src/data-gitlab-project-ids/index.ts
+++ src/data-gitlab-project-ids/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/gitlabhq/gitlab/17.8.0/docs/data-sources/project_ids
 // generated from terraform resource schema
 
