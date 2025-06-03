83c10aab1edbc58c9b5c7e9d6e089a41ad3d6318
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2025-06-03 15:40:45 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/global-level-notifications/index.ts
+++ src/global-level-notifications/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/gitlabhq/gitlab/18.0.0/docs/resources/global_level_notifications
 // generated from terraform resource schema
 
